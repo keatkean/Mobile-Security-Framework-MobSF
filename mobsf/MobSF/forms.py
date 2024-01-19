bc38b0666c28a5c67261e2a76c2f5068ea50a93a
xinying <90682369+x-ny-ng@users.noreply.github.com>
2024-01-19 11:29:50 +0800

    added form for automated analysis

--- mobsf/MobSF/forms.py
+++ mobsf/MobSF/forms.py
@@ -34,3 +34,10 @@ class FormUtil(object):
     @staticmethod
     def errors(form):
         return form.errors.get_json_data()
+    
+class NameForm(forms.Form):
+    type_CHOICES = {'static': 'Static Analysis', 'dynamic': 'Dynamic Analysis'}
+    type = forms.ChoiceField(label='Analysis Type', choices=type_CHOICES)
+    input_path = forms.CharField(label="File or folder path", max_length=10000)
+    activities_CHOICES = {0: "Don't run android activites", 1: "Run android activities"}
+    androidactivities = forms.ChoiceField(label='Android Activities', choices=activities_CHOICES)
