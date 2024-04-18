681cba6be52bf66abb3f60b1fc0c69a1dfbc3e3c
Xghub23 <99709651+Xghub23@users.noreply.github.com>
2024-04-18 17:17:44 +0800

    Updated environment.py
    Added def execute_accessibility_commands and def check_accessibility. Changed API limit to 34 and removed the from getaccessibility import *

--- mobsf/DynamicAnalyzer/views/android/environment.py
+++ mobsf/DynamicAnalyzer/views/android/environment.py
@@ -34,10 +34,8 @@ from mobsf.MobSF.utils import (
 )
 from mobsf.StaticAnalyzer.models import StaticAnalyzerAndroid
 
-from getaccessibility import *
-
 logger = logging.getLogger(__name__)
-ANDROID_API_SUPPORTED = 29
+ANDROID_API_SUPPORTED = 34
 
 
 class Environment:
@@ -156,6 +154,36 @@ class Environment:
         # Verify Installation
         return self.is_package_installed(package, out.decode('utf-8', 'ignore')), out.decode('utf-8', 'ignore')
 
+    def check_accessibility_permission(self, package):
+        """Check if APK has BIND_ACCESSIBILITY_SERVICE permission."""
+        try:
+            out = self.adb_command(['pm', 'dump', package], True)
+            if out is None:
+                logger.error('Error executing adb command to check permissions')
+                return False
+            return b'BIND_ACCESSIBILITY_SERVICE' in out
+        except Exception:
+            logger.exception('Error checking accessibility permission')
+            return False
+
+    def execute_accessibility_commands(self):
+        """Execute commands to open Accessibility settings and navigate."""
+        logger.info('Opening Accessibility settings')
+        self.adb_command(['shell', 'am', 'start', '-a', 'android.settings.ACCESSIBILITY_SETTINGS'])
+        time.sleep(2)  # Wait for the settings activity to open
+        logger.info('Navigating through Accessibility settings')
+        # Simulate key events to navigate through the Accessibility settings
+        commands = [
+            'KEYCODE_TAB',
+            'KEYCODE_ENTER',
+            'KEYCODE_ENTER',
+            'KEYCODE_TAB',
+            'KEYCODE_ENTER',
+        ]
+        for command in commands:
+            self.adb_command(['shell', 'input', 'keyevent', command])
+            time.sleep(1)  # Add a delay between key events
+    
     def adb_command(self, cmd_list, shell=False, silent=False):
         """ADB Command wrapper."""
         args = [get_adb(),
